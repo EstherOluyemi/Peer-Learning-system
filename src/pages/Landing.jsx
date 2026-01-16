c5845c44abcf4c2bc43943e62b6ebcc231ff8110
EstherOluyemi <estheroluyemi25@gmail.com>
2026-01-16 17:31:39 +0100

    Setup complete

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+   <div className='min-h-screen bg-gray-50 w-full '>
+    <h1 className='bg-red-400'>Welcome to the Landing Page</h1>
+    <p className='text-purple-500'>Tailwind is working</p>
+   </div>
+  )
+}
+
+export default Landing
